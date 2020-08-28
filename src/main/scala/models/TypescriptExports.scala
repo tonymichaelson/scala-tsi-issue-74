4093a90e3befaf8471e1b994ca7166a195d98349
Anthony Michaelson <tonymichaelson@protonmail.com>
2020-08-28 08:30:48 -0700

    Use case for enumeratum enums to Typescript

---
+++ src/main/scala/models/TypescriptExports.scala
@@ -0,0 +1,15 @@
+package models
+
+import com.scalatsi.TypescriptType.{TSLiteralString, TSUnion}
+import com.scalatsi._
+
+object TypescriptExports extends DefaultTSTypes {
+  // maps
+  def enumTsType[E <: enumeratum.EnumEntry, AllValues <: enumeratum.Enum[E]: Manifest](enum: AllValues)(
+    implicit m: Manifest[AllValues]): TSType[E] =
+    TSType.alias[E](
+      m.runtimeClass.getSimpleName.stripSuffix("$"),
+      TSUnion(enum.values.map(e => TSLiteralString(e.entryName)))
+    )
+  implicit val myEnumTsType: TSType[MapMode] = enumTsType(MapMode)
+}
