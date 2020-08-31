104334c4264c2fb613dfea6f2cf135618d57b4b7
Anthony Michaelson <tonymichaelson@protonmail.com>
2020-08-31 14:01:47 -0700

    David identified the fix was to use TSNamedType vs TSType

--- models.ts
+++ models.ts
@@ -1,19 +1,7 @@
-export type FunkyMapMode = (IMAP | ILIST | IDOCUMENT)
+export type FunkyMapMode = ("MAP" | "LIST" | "DOCUMENT")
 
 export interface ICaseClass {
   mode: MapMode
-}
-
-export interface IDOCUMENT {
-
-}
-
-export interface ILIST {
-
-}
-
-export interface IMAP {
-
 }
 
 export type MapMode = ("MAP" | "LIST" | "DOCUMENT")
--- src/main/scala/models/TypescriptExports.scala
+++ src/main/scala/models/TypescriptExports.scala
@@ -6,10 +6,11 @@ import com.scalatsi._
 object TypescriptExports extends DefaultTSTypes {
   // maps
   def enumTsType[E <: enumeratum.EnumEntry, AllValues <: enumeratum.Enum[E]: Manifest](enum: AllValues)(
-    implicit m: Manifest[AllValues]): TSType[E] =
+    implicit m: Manifest[AllValues]): TSNamedType[E] =
     TSType.alias[E](
       m.runtimeClass.getSimpleName.stripSuffix("$"),
       TSUnion(enum.values.map(e => TSLiteralString(e.entryName)))
     )
-  implicit val myEnumTsType: TSType[MapMode] = enumTsType(MapMode)
+  implicit val funkyMapMode: TSNamedType[FunkyMapMode] = enumTsType(FunkyMapMode)
+  implicit val mapMode     : TSNamedType[MapMode]      = enumTsType(MapMode)
 }
